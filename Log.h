8bd55c4235962bbeef6e5364c53bedbab26b792c
root <1505243015@qq.com>
2022-03-25 00:06:11 +0800

    update 添加了logformatter和 logAppender类

--- Log.h
+++ Log.h
@@ -10,6 +10,7 @@
 
 #include <string>
 #include <memory>
+#include <vector>
 #include <stddef.h>
 namespace CatLog{
     class Logger;
@@ -128,6 +129,151 @@ namespace CatLog{
             LogEvent::ptr m_event;
     };
     
+    /**
+     * @brief 日志格式化
+     */
+    class LogFormatter{
+        public:
+            typedef std::shared_ptr<LogFormatter> ptr;
+            /**
+             * @brief 构造函数
+             * @param[in] pattern 格式模板
+             * @details
+             * %m 消息
+             * %p 日志级别
+             * %r 累计毫秒数
+             * %c 日志名称
+             * %t 线程id
+             * %n 换行
+             * %d 时间
+             * %f 文件名
+             * %l 行号
+             * %T 制表符
+             * %F 协程id
+             * %N 线程名称
+             * 
+             * 默认格式 "%d{%Y-%m-%d %H:%M:%S}%T%t%T%N%T%F%T[%p]%T[%c]%T%f:%l%T%m%n"
+             */
+            LogFormatter(const std::string& pattern);
+
+            /**
+             * @brief 返回格式化日志文本
+             * @param[in] logger 日志器
+             * @param[in] level  日志级别
+             * @param[in] event  日志事件
+             */
+            std::string format(std::shared_ptr<Logger> logger,LogLevel::Level level,LogEvent::ptr event);
+            std::ostream& format(std::ostream& ofs,std::shared_ptr<Logger> logger,LogLevel::Level level,
+                LogEvent::ptr event);
+        public:
+            /**
+             * @brief 日志内容项格式化
+             */
+            class FormatItem{
+                public:
+                    typedef std::shared_ptr<FormatItem> ptr;
+                    /**
+                     * @brief 析构函数
+                     */
+                    virtual ~FormatItem();
+                    /**
+                     * @brief 格式化日志到流
+                     * @param[in,out] os 日志输出流
+                     * @param[in] logger 日志器
+                     * @param[in] level 日志等级
+                     * @param[in] event 日志事件
+                     */
+                    virtual void format(std::ostream& os,std::shared_ptr<Logger> logger,
+                        LogLevel::Level level,LogEvent::ptr event) = 0;
+            };
+            /**
+             * @brief 初始化，解析日志模板
+             */
+            void init();
+
+            /**
+             * @brief 是否有错误
+             */
+            bool isError() const {return m_error;}
+
+            /**
+             * @brief 返回日志模板
+             */
+            const std::string getPattern() const {return m_pattern;}
+
+        private:
+            // 日志格式模板
+            std::string m_pattern;
+            // 日志格式解析后格式
+            std::vector<FormatItem::ptr> m_items;
+            // 是否有错误
+            bool m_error = false;
+    };
+
+    /**
+     * @brief 日志输出目标
+     */
+    class LogAppender{
+        friend class Logger;
+        public:
+            typedef std::shared_ptr<LogAppender> ptr;
+            typedef SpinLock MutexType;
+
+            /**
+             * @brief 析构函数
+             */
+            virtual ~LogAppender(){}
+
+            /**
+             * @brief 写入日志
+             * @param[in] logger 日志器
+             * @param[in] level  日志级别
+             * @param[in] event  日志事件
+             */
+            virtual void log(std::shared_ptr<Logger> logger,LogLevel::Level level,
+                LogEvent::ptr event)=0;
+            
+            /**
+             * @brief 将日志输出目标的配置转成YAML String
+             */
+            virtual std::string toYamlString() = 0;
+
+            /**
+             * @brief 更改日志器
+             */
+            void setFormatter(LogFormatter::ptr val);
+
+            /**
+             * @brief 获取日志格式器
+             */
+            LogFormatter::ptr getFormatter();
+
+            /**
+             * @brief 获取日志级别
+             */
+            LogLevel::Level getLevel() const {return m_level;}
+
+            /**
+             * @brief 设置日志级别
+             */
+            void setLevel(LogLevel::Level val) {m_level = val;}
+
+        protected:
+            // 日志级别
+            LogLevel::Level m_level = LogLevel::DEBUG;
+            // 是否有自己的日志格式器
+            bool m_hasFormatter = false;
+            // Mutex
+            MutexType m_mutex;
+            // 日志格式器
+            LogFormatter::ptr m_formatter;            
+    };
+
+    /**
+     * @brief 日志器
+     */
+    
+
 
 }
 
